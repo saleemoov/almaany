d22b3a9d6791bf2688bf41fd6b81f650bd909d38
saleemoov <almaanysaleem@gmail.com>
2026-04-13 21:12:16 +0000

    clean slate

--- main.py
+++
@@ -1,8 +0,0 @@
-from logger import setup_logger
-from trading_bot import TradingBot
-
-if __name__ == "__main__":
-    setup_logger()
-    import asyncio
-    bot = TradingBot()
-    asyncio.run(bot.start())
